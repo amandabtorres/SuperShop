97eec1055fd6507c0a686cf98f55a7ed2f8ab889
amandabtorres <banaszeskiamanda@gmail.com>
2024-09-09 15:47:09 +0100

    Repository: City and Country

---
+++ SuperShop/Data/CountryRepository.cs
@@ -0,0 +1,14 @@
+﻿using SuperShop.Data.Entities;
+
+namespace SuperShop.Data
+{
+    public class CountryRepository : GenericRepository<Country>, ICountryRepository
+    {
+       
+
+        public CountryRepository(DataContext context) : base(context)
+        {
+            
+        }
+    }
+}
