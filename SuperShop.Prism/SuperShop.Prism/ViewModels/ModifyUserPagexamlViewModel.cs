3cac5a75e544aa9ed86433bcb0ca866e90547dda
amandabtorres <banaszeskiamanda@gmail.com>
2024-09-26 18:38:55 +0100

    Master Detail page

---
+++ SuperShop.Prism/SuperShop.Prism/ViewModels/ModifyUserPagexamlViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using SuperShop.Prism.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SuperShop.Prism.ViewModels
+{
+	public class ModifyUserPagexamlViewModel : ViewModelBase
+	{
+        public ModifyUserPagexamlViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Title = Languages.ModifyUser;
+
+        }
+	}
+}
---
+++ SuperShop.Prism/SuperShop.Prism/Views/ModifyUserPagexaml.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace SuperShop.Prism.Views
+{
+    public partial class ModifyUserPagexaml : ContentPage
+    {
+        public ModifyUserPagexaml()
+        {
+            InitializeComponent();
+        }
+    }
+}
